dafa74cca4ceb875f3b13065ba7112314c1c3808
Randy Brown <thatrandybrown@gmail.com>
2025-08-08 17:04:37 -0500

    add a connection class for requesting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import http.client
 import sys
 from urllib.parse import urlparse
 
@@ -8,6 +9,8 @@ def main():
         print(f" - {arg}")
     parsed = urlparse(sys.argv[1] if len(sys.argv) > 1 else "")
     print(f"Parsed URL: {parsed}")
+    conn_class = http.client.HTTPSConnection if parsed.scheme == "https" else http.client.HTTPConnection
+    conn = conn_class(parsed.netloc)
 
 if __name__ == "__main__":
     main()
